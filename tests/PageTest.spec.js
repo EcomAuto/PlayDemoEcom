754805061409df6cfcdf5175befa48657e59e5c5
EcomAuto <mishr.ajp654@gmail.com>
2025-07-06 21:13:55 +0530

    title fetch

--- tests/PageTest.spec.js
+++ tests/PageTest.spec.js
@@ -4,6 +4,9 @@ test('Page Tset', async({page}) =>{
 
     await page.goto('https://www.flipkart.com/');
 
+    const pageTitle = await page.title();
+    console.log(`Page Title: ${pageTitle}`);
+
     await page.close();
  
 })

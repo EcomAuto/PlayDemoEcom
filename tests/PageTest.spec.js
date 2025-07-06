ce7ab7a8cab0b1ec803495510a62262deb2f9f68
EcomAuto <mishr.ajp654@gmail.com>
2025-07-06 19:06:38 +0530

    initial commit

---
+++ tests/PageTest.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+
+test('Page Tset', async({page}) =>{
+
+    await page.goto('https://www.flipkart.com/');
+    
+    await page.close();
+
+})
